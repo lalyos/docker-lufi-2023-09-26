3e160dc4468bf0229ed6c82c24e95758c3ef23e5
lalyos <lalyos@yahoo.com>
2023-09-26 15:15:25 +0200

    use real html

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo ${TITLE:=Welcome} > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>${TITLE:=Welcome}</h1>
+</body>
+</html>
+EOF
 
 nginx -g "daemon off;"
